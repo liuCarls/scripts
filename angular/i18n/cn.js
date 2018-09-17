bffc3594afb6c5fac7f58d64b91afa699a279eac
liugx <liugx618@163.com>
2018-09-17 09:59:14 +0800

    脚本 demo

---
+++ angular/i18n/cn.js
@@ -0,0 +1,11 @@
+var cnObj = {
+	"login": "登录",
+	"regist": "注册",
+	"user": {
+		"name": "名字",
+		"sex": "性别",
+		"age": "年龄",
+		"addr": "地址"
+	}
+	
+}
---
+++ angular/i18n/en.js
@@ -0,0 +1,10 @@
+var enObj={
+	"login":"Login",
+	"regist":"Register",
+	"user": {
+		"name": "Name",
+		"sex": "Sex",
+		"age": "Age",
+		"addr": "Address"
+	}
+}
