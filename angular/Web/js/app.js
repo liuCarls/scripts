a7b11e3dfa2434868f39e22fa82eb8c11d885a05
liugx <liugx618@163.com>
2018-12-25 14:38:17 +0800

    angularjs Tab

---
+++ angular/Web/js/app.js
@@ -0,0 +1,21 @@
+/**
+ * Created by user on 2018/12/1.
+ */
+'use strict';
+
+angular.module('ngWeb', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch',
+    'ngStorage',
+    'ui.router',
+    'ui.bootstrap',
+    'ui.utils',
+    'ui.load',
+    'ui.jq',
+    'oc.lazyLoad',
+    'pascalprecht.translate',
+    'ngScrollbars'
+]);
---
+++ angular/Web/js/config.js
@@ -0,0 +1,10 @@
+/**
+ * Created by user on 2018/12/1.
+ */
+
+
+var app = angular.module('ngWeb')
+    .config()
+    // .constant()
+    // .factory()
+    ;
